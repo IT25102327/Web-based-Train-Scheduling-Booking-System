e929ac17e8d2e7cb3f0c02a2bc06a43751801f5d
IT25102327 <IT25102327@my.sliit.lk>
2026-09-22 11:53:06 +0530

    Setup models - make schedule and train model

--- cm.ps1
+++ cm.ps1
@@ -9,7 +9,7 @@ git add .
 
 
 # Commit with the automatically generated message.
-git commit -m "Setup models - make route"
+git commit -m "Setup models - make schedule and train model"
 
 # Push the changes to the 'IT25102327' branch of the 'origin' remote.
 git push -u origin IT25102327
